44fbae9d76d12074d6cdaba9c81ed925b10e7452
vattgern <sashawot060@gmail.com>
2022-11-07 15:39:36 +0400

    Created Auth working in PostMan 7/11/22

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\API\LoginRequest;
+use App\Http\Requests\API\RegisterRequest;
+
+class AuthController extends Controller
+{
+    public function registration(RegisterRequest $request){
+        // Создание пользователя
+        // -----------------------
+        $user = User::create([
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+        // -----------------------
+        //Создается токен и сразу в базу
+        // -----------------------
+        $token = $user->createToken('auth_token')->plainTextToken;
+        // -----------------------
+        // Sanctum подключение
+        // -----------------------
+        //auth('sanctum')->login($user);
+        // -----------------------
+        // Отправка ответа JSON
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'code' => 200,
+        ]);
+    }
+
+    public function login(LoginRequest $request){
+        // Проверка что пользователь существует
+        // -----------------------
+        if(!Auth::attempt($request->all())){
+            return response()->json([
+                'msg' => 'Неверные данные',
+                'code' => 401
+            ]);
+        }
+        // Подключение Auth
+        // -----------------------
+        $user = Auth::user();
+        // Создание токена
+        // -----------------------
+        $token = $user->createToken('auth_token')->plainTextToken;
+        // Отправка отвера в JSON
+        // -----------------------
+        return response()->json([
+            'code' => 200,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+    public function me(){
+        return Auth::user();
+    }
+    public function logout(){
+        //Удаление токенов
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'msg' => 'Вы вышли'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthController::class,'me']);
+    Route::post('/logout',[AuthController::class,'logout']);
 });
+
+// Авторизация&Регистрация
+Route::post('/reg', [AuthController::class,'registration']);
+Route::post('/login',[AuthController::class,'login'])->name('login');
